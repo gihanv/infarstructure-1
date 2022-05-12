3ff88da87d2012c54cba7fa0a16ed2c1de83a70b
Gihan Vithana <79891523+gihanv@users.noreply.github.com>
2022-05-12 13:26:03 +0530

    Update main.tf
    Status fails updated

--- terraform/modules/security_group/main.tf
+++ terraform/modules/security_group/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "private" {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    security_groups = [aws_security_group.public.id]
+    security_groups = ["0.0.0.0/0"]
   }
 depends_on = [
   aws_security_group.public
